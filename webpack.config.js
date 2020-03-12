2c03bfa2a063575ffb13c59d5dd38485f1ad1be5
Milo Casagrande <milo@foundries.io>
2020-03-12 10:59:23 +0100

    Add license headers
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+/*
+Copyright 2020 Foundries.IO Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+*/
+
 const path = require('path');
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
